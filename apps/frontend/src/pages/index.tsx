31ee60a1e768f0daba91dc9613a2917ff4fb01d0
DanielCG <danielcgilibert@gmail.com>
2023-05-04 10:34:06 +0200

    ✨ feat(index.tsx): add dice game functionality to the page, including rolling dice, displaying them on the screen, and updating the game state. Also, add socket.io functionality to the page to receive and display messages from the server.

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,112 +1,112 @@
-import Image from 'next/image'
+import { socket } from '@/socket'
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [player1Dice, setPlayer1Dice] = useState(0)
+  const [player2Dice, setPlayer2Dice] = useState(0)
+  const [game, setGame] = useState([
+    [
+      [0, 0, 0],
+      [5, 0, 0],
+      [0, 0, 0],
+    ],
+    [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ],
+  ])
+  const [isConnected, setIsConnected] = useState(socket.connected)
+  const [messages, setMessages] = useState<String[]>([])
+
+  const handleRoll = () => Math.floor(Math.random() * 6) + 1
+
+  const setNumberDice = (
+    player: number,
+    number: number,
+    indexRow: number,
+    indexCell: number
+  ) => {
+    if (player === 1) {
+      setPlayer1Dice(number)
+      game.map((player, indexPlayer) => {
+        console.log(player)
+      })
+    }
+
+    if (player === 2) {
+      setPlayer2Dice(number)
+    }
+  }
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true)
+    }
+    const receiveMessage = (message: string) => {
+      console.log(message)
+      setMessages([message, ...messages])
+    }
+    function onDisconnect() {
+      setIsConnected(false)
+    }
+
+    socket.on('connect', onConnect)
+    socket.on('message', receiveMessage)
+    socket.on('disconnect', onDisconnect)
+    socket.emit('new-message', 'message from frontend')
+
+    return () => {
+      socket.off('connect', onConnect)
+      socket.off('disconnect', onDisconnect)
+    }
+  }, [])
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer">
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      className={`grid place-content-center min-h-screen text-center ${inter.className}`}>
+      <h1 className="text-4xl font-bold mb-5">Dices</h1>
+      <div className="flex">
+        <div className="border-2 mb-auto p-5">dado {player1Dice}</div>
+        <div className="grid gap-5 text-center">
+          {game.map((player, indexPlayer) => (
+            <div key={indexPlayer}>
+              <h1 className="mb-5">Player {indexPlayer + 1}</h1>
+
+              {player.map((row, indexRow) => (
+                <>
+                  <div className="flex gap-5 " key={indexPlayer}>
+                    {row.map((cell, indexCell) => (
+                      <div
+                        onClick={() => {
+                          setNumberDice(
+                            indexPlayer + 1,
+                            handleRoll(),
+                            indexRow,
+                            indexCell
+                          )
+                          console.log(player1Dice)
+                        }}
+                        className="cursor-pointer hover:bg-slate-500 w-28 h-24 border-2 flex justify-center items-center m-1"
+                        key={indexCell}>
+                        {cell}
+                      </div>
+                    ))}
+                  </div>
+                </>
+              ))}
+            </div>
+          ))}
         </div>
+        <div className="border-2  p-5 mt-auto">dado {player2Dice}</div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button onClick={handleRoll} className="border-2 p-4 mt-5">
+        Start Game
+      </button>
     </main>
   )
 }
