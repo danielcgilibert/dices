f32bc6fa187d51c44a955fcaf48f95a84ff9df91
DanielCG <danielcgilibert@gmail.com>
2023-05-04 10:32:51 +0200

    ✨ feat(server.js): add support for socket.io to enable real-time communication between server and client 🐛 fix(server.js): change hard-coded port number to use PORT variable from config.js file to improve configurability and semantics 🎨 style(server.js): add morgan and cors middleware to improve logging and enable cross-origin resource sharing

--- apps/server/server.js
+++ apps/server/server.js
@@ -1,11 +1,37 @@
 import express from 'express'
+import morgan from 'morgan'
+import { Server as SocketServer } from 'socket.io'
+import http from 'http'
+import cors from 'cors'
+import { PORT } from './config.js'
 
 const app = express()
+const server = http.createServer(app)
+const io = new SocketServer(server, {
+  cors: {
+    origin: '*',
+  },
+})
+
+app.use(morgan('dev'))
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3005, () => {
-  console.log('Server is running on port 3005')
+io.on('connection', socket => {
+  // send a message to the client
+  socket.emit('message', ['hola', 'mundo'])
+
+  // receive a message from the client
+  socket.on('new-message', (...args) => {
+    console.log('entra')
+
+    console.log('hello from client', args)
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
